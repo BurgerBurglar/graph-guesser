36b7fd5249a3709ca072aa822609bcff48b7e83a
Shuo Tian <tianshuo1996@outlook.com>
2024-04-12 19:56:24 -0500

    front page facelift

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,19 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-green-100 to-green-50">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <Image src="/front-page.webp" alt="logo" width={250} height={250} />
-        <h1 className="text-5xl font-extrabold tracking-tight text-green-600 sm:text-[5rem]">
-          <span className="text-blue-600">Graph</span> Guesser
+        <h1 className="tracking-tightsm:text-[5rem] text-5xl font-extrabold">
+          <span className="text-blue-600">Graph</span>{" "}
+          <span className="text-green-600 ">Guesser</span>
         </h1>
-        <div className="text-center text-lg">{`We have the plots. You can guess what they mean.`}</div>
+        <div className="text-center text-lg font-medium text-green-800">
+          <p>{`We have the plots.`}</p>
+          <p>{`You can guess what they mean.`}</p>
+        </div>
         <PrimaryButton
           className="p-6 text-2xl font-bold"
           onClick={playRandomGame}
         >
-          {`Let's Play`}
+          {`LET'S GUESS`}
         </PrimaryButton>
       </div>
     </main>
