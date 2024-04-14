c6ae339d4f322cbf78ca6514b024c51a886592e8
Shuo Tian <tianshuo1996@outlook.com>
2024-04-14 12:19:05 -0500

    arrange buttons horiztontally

--- src/app/all-done/page.tsx
+++ src/app/all-done/page.tsx
@@ -12,23 +12,25 @@ const AllDone: NextPage = () => {
   const { playRandomGame } = usePlay();
 
   return (
-    <main className="container relative flex min-h-[100dvh] flex-col justify-between gap-6 bg-gradient-to-b px-4 pb-6 pt-16">
-      <CloseButton />
-      <div className="flex flex-col items-center gap-4">
-        <Image src="/all-done.webp" alt="all-done" width={250} height={250} />
-        <div className="text-4xl font-bold text-green-700">ALL DONE</div>
-        <div className="text-center font-medium">
-          <p>You finished all quizes.</p>
-          <p>Want to play the old ones again?</p>
+    <div className="bg-gradient-to-b from-green-100 to-green-50">
+      <main className="container relative flex min-h-[100dvh] flex-col justify-between gap-6 bg-gradient-to-b px-4 pb-6 pt-16">
+        <CloseButton />
+        <div className="flex flex-col items-center gap-4">
+          <Image src="/all-done.webp" alt="all-done" width={250} height={250} />
+          <div className="text-4xl font-bold text-green-700">ALL DONE</div>
+          <div className="text-center font-medium">
+            <p>You finished all quizes.</p>
+            <p>Want to play the old ones again?</p>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-2">
-        <NeutralButton>SHARE</NeutralButton>
-        <PrimaryButton onClick={() => playRandomGame(true)}>
-          CONTINUE
-        </PrimaryButton>
-      </div>
-    </main>
+        <div className="flex flex-col gap-2 md:flex-row">
+          <NeutralButton className="grow">SHARE</NeutralButton>
+          <PrimaryButton className="grow" onClick={() => playRandomGame(true)}>
+            CONTINUE
+          </PrimaryButton>
+        </div>
+      </main>
+    </div>
   );
 };
 
--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -22,46 +22,48 @@ const Results: NextPage = () => {
   const isClient = useIsClient();
 
   return (
-    <main className="container relative flex h-[100dvh] min-h-[511px] flex-col items-center justify-between gap-6 bg-gradient-to-b px-4 pb-6 pt-16">
-      <CloseButton />
-      <div className="flex min-h-[141.6px] grow flex-col items-center gap-2">
-        <div className="relative max-h-[350px] min-h-[100px] w-full max-w-[350px] basis-[350px]">
-          <Image
-            src="/you-did-great.webp"
-            alt="you did great!"
-            fill
-            objectFit="contain"
-          />
-        </div>
-        <div className="text-center font-medium">
-          <p>You learned so much about the world today!</p>
-          <p>Wanna learn more?</p>
-        </div>
-        <div className="flex gap-4">
-          {isClient && !!quizIds.length && (
-            <ResultDisplay
-              header="JUST NOW"
-              numCorrect={numCorrectResultsInDeck}
-              numTotal={quizIds.length}
+    <div className="bg-gradient-to-b from-green-100 to-green-50">
+      <main className="container relative flex h-[100dvh] min-h-[511px] flex-col items-center justify-between gap-6 bg-gradient-to-b px-4 pb-6 pt-16">
+        <CloseButton />
+        <div className="flex min-h-[141.6px] grow flex-col items-center gap-2">
+          <div className="relative max-h-[350px] min-h-[100px] w-full max-w-[350px] basis-[350px]">
+            <Image
+              src="/you-did-great.webp"
+              alt="you did great!"
+              fill
+              objectFit="contain"
             />
-          )}
-          {isClient && !!numResults && (
-            <ResultDisplay
-              header="OVERALL"
-              numCorrect={numCorrectResults}
-              numTotal={numResults}
-            />
-          )}
+          </div>
+          <div className="text-center font-medium">
+            <p>You learned so much about the world today!</p>
+            <p>Wanna learn more?</p>
+          </div>
+          <div className="flex gap-4">
+            {isClient && !!quizIds.length && (
+              <ResultDisplay
+                header="JUST NOW"
+                numCorrect={numCorrectResultsInDeck}
+                numTotal={quizIds.length}
+              />
+            )}
+            {isClient && !!numResults && (
+              <ResultDisplay
+                header="OVERALL"
+                numCorrect={numCorrectResults}
+                numTotal={numResults}
+              />
+            )}
+          </div>
         </div>
-      </div>
-      <div className="flex w-full flex-col items-stretch gap-2">
-        <NeutralButton>SHARE</NeutralButton>
-        <NeutralButton>SEE RESULTS</NeutralButton>
-        <PrimaryButton onClick={() => playRandomGame(false)}>
-          PLAY AGAIN
-        </PrimaryButton>
-      </div>
-    </main>
+        <div className="flex w-full flex-col items-stretch gap-2 md:flex-row">
+          <NeutralButton className="grow">SHARE</NeutralButton>
+          <NeutralButton className="grow">SEE RESULTS</NeutralButton>
+          <PrimaryButton className="grow" onClick={() => playRandomGame(false)}>
+            PLAY AGAIN
+          </PrimaryButton>
+        </div>
+      </main>
+    </div>
   );
 };
 export default Results;
