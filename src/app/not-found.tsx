6c6e03a34e5ce83965e4c3106316a7a321420ef1
Shuo Tian <tianshuo1996@outlook.com>
2024-05-27 20:16:39 -0500

    update CSS for not found and home

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,23 +5,19 @@ import { Button, CloseButton } from "~/components/ui/button";
 const NotFound: NextPage = () => {
   return (
     <div className="flex flex-col justify-end">
-      <main className="container relative flex h-[100dvh] flex-col items-center justify-end gap-6 px-4 pb-6 pt-16">
+      <main className="container relative flex h-[100dvh] flex-col items-center justify-between px-4 pb-6 pt-16">
         <CloseButton />
-        <div className="flex max-h-[500px] min-h-[200px] grow flex-col gap-4">
-          <div className="flex min-h-[141.6px] grow flex-col items-center gap-4">
-            <div className="relative max-h-[250px] min-h-[100px] w-full max-w-[250px] shrink basis-[250px]">
-              <Image src="/404.webp" alt="not found" fill objectFit="contain" />
-            </div>
-            <div className="text-xl font-bold">{`ERROR 404`}</div>
-            <div className="text-center font-medium">
-              {`Sorry, the page you're looking for doesn't exist`}
-            </div>
+        <div className="flex flex-col items-center gap-4">
+          <Image src="/404.webp" alt="not found" width={250} height={250} />
+          <div className="text-4xl font-bold text-green-700">{`ERROR 404`}</div>
+          <div className="text-center font-medium">
+            {`Sorry, the page you're looking for doesn't exist`}
           </div>
-          <div className="flex w-full flex-col items-stretch gap-2 md:flex-row">
-            <Button asChild variant="primary">
-              <Link href="/">GO HOME</Link>
-            </Button>
-          </div>
+        </div>
+        <div className="flex w-full flex-col items-stretch gap-2 md:flex-row">
+          <Button asChild variant="primary">
+            <Link href="/">GO HOME</Link>
+          </Button>
         </div>
       </main>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,27 +10,25 @@ export default function HomePage() {
   const { playRandomGame } = usePlay();
 
   return (
-    <main className="flex min-h-[100dvh] flex-col items-center justify-center">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <Image src="/front-page.webp" alt="logo" width={250} height={250} />
-        <h1 className="tracking-tightsm:text-[5rem] text-5xl font-extrabold">
-          <div className="-translate-x-6 text-blue-700">GRAPH</div>
-          <div className="translate-x-6 text-green-700">GUESSER</div>
-        </h1>
-        <div className="text-center text-lg font-medium text-green-800">
-          <p>{`We have the plots.`}</p>
-          <p>{`You can guess what they mean.`}</p>
-        </div>
-        <div className="flex w-full flex-col gap-2">
-          <Button variant="primary" onClick={() => playRandomGame(false)}>
-            {`LET'S GUESS`}
+    <main className="container flex min-h-[100dvh] flex-col items-center justify-center gap-6 px-4 py-16">
+      <Image src="/front-page.webp" alt="logo" width={250} height={250} />
+      <h1 className="tracking-tightsm:text-[5rem] text-5xl font-extrabold">
+        <div className="-translate-x-6 text-blue-700">GRAPH</div>
+        <div className="translate-x-6 text-green-700">GUESSER</div>
+      </h1>
+      <div className="text-center text-lg font-medium text-green-800">
+        <p>{`We have the plots.`}</p>
+        <p>{`You can guess what they mean.`}</p>
+      </div>
+      <div className="bottom-8 flex w-full flex-col gap-2">
+        <Button variant="primary" onClick={() => playRandomGame(false)}>
+          {`LET'S GUESS`}
+        </Button>
+        {DISPLAY_EXPORE_BUTTON && (
+          <Button asChild variant="neutral">
+            <Link href="/explore">EXPLORE</Link>
           </Button>
-          {DISPLAY_EXPORE_BUTTON && (
-            <Button asChild variant="neutral">
-              <Link href="/explore">EXPLORE</Link>
-            </Button>
-          )}
-        </div>
+        )}
       </div>
     </main>
   );
