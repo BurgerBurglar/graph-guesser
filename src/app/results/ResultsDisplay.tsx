036c9d5a8fb2ba8452db11592e457c5d2de0fd44
Shuo Tian <tianshuo1996@outlook.com>
2024-11-15 14:09:30 -0600

    fix css for results display

--- src/app/results/ResultsDisplay.tsx
+++ src/app/results/ResultsDisplay.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 interface ResultDisplayProps {
   header: string;
   numCorrect: number;
@@ -13,7 +11,7 @@ const ResultDisplay: React.FC<ResultDisplayProps> = ({
 }) => {
   return (
     <div className="flex flex-col items-stretch rounded-xl border-2 border-t-0 border-green-700 text-center">
-      <div className="min-w-32 rounded-t-xl bg-green-700 py-1 font-bold text-white">
+      <div className="min-w-32 rounded-t-[10px] bg-green-700 py-1 font-bold text-white">
         {header}
       </div>
       <div className="px-10 py-2 text-lg font-medium text-green-700">
