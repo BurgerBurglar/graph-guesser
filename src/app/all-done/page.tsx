10ff57325f88b29e5092757c5252b9aabf2a34a4
Shuo Tian <tianshuo1996@outlook.com>
2024-11-28 13:07:41 -0600

    add confirm dialog for clear record

--- src/app/all-done/page.tsx
+++ src/app/all-done/page.tsx
@@ -5,6 +5,16 @@ import Image from "next/image"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { Button, CloseButton } from "~/components/ui/button"
+import {
+  DialogHeader,
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+  DialogClose,
+} from "~/components/ui/dialog"
 import { usePlay } from "~/hooks"
 import { useAppStore } from "~/lib/zustand"
 
@@ -35,16 +45,41 @@ const AllDone: NextPage = () => {
         <Button variant="outline" className="flex-1" asChild>
           <Link href="/">CHANGE DIFFICULTY</Link>
         </Button>
-        <Button
-          variant="outline"
-          className="flex-1"
-          onClick={() => {
-            resestResults()
-            void router.push("/")
-          }}
-        >
-          CLEAR RECORD
-        </Button>
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" className="flex-1">
+              CLEAR RECORD
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Do you want to clear all record?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone; it will permanently delete your
+                data from your browser.
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter className="gap-2">
+              <DialogClose asChild>
+                <Button variant="outline" className="flex-1">
+                  DO NOT CLEAR
+                </Button>
+              </DialogClose>
+              <Button
+                variant="destructive"
+                className="flex-1"
+                onClick={() => {
+                  resestResults()
+                  void router.push("/")
+                }}
+              >
+                CLEAR RECORD
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+
         <Button
           variant="primary"
           className="flex-1"
