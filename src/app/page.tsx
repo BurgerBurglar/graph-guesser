f5b65e965ba4404c80ac63b04aabc5b863fcda95
Shuo Tian <shuo.tian@goosehead.com>
2024-12-02 15:37:10 -0600

    fix frontpage scrolling bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import dynamic from "next/dynamic"
-import Image from "next/image"
-import NavLinks from "~/app/NavLinks"
-import ButtonLoading from "~/app/quizes/[id]/ButtonLoading"
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import NavLinks from "~/app/NavLinks";
+import ButtonLoading from "~/app/quizes/[id]/ButtonLoading";
 const LetsGuess = dynamic(() => import("./LetsGuess"), {
   ssr: false,
   loading: ButtonLoading,
-})
+});
 export default function HomePage() {
   return (
     <main className="container flex min-h-[100dvh] flex-col items-center justify-end gap-4 p-4 pb-6">
       <NavLinks />
       <div className="flex grow flex-col items-center gap-6">
-        <div className="relative max-h-[265px] min-h-[141.6px] w-full grow">
+        <div className="relative max-h-[265px] min-h-[110px] w-full grow">
           <Image
             src="/front-page.webp"
             alt="logo"
@@ -33,5 +33,5 @@ export default function HomePage() {
         <LetsGuess />
       </div>
     </main>
-  )
+  );
 }
