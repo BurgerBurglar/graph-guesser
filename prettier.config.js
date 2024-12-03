d4d95b748103b71fe5b7e1a83563b29af2cc2014
Shuo Tian <tianshuo1996@outlook.com>
2024-12-02 18:13:53 -0600

    update formating

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,6 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
-  semi: false,
-};
+}
 
-export default config;
+export default config
